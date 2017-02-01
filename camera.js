4dffe3bc820c76b20f9ca4e09fa7c8c458ddc96a
TJ Woon <tj@cloudsky.org>
2017-02-01 22:10:43 +0800

    Reimplement image compositing using the JIMP library
    Hopefully this is faster.

--- camera.js
+++ camera.js
@@ -2,6 +2,7 @@
 
 // Imports ---------------------------------------------------------------------
 
+const Jimp = require("jimp")
 const Q = require("q")
 const sharp = require("sharp")
 const v4l2camera = require("v4l2camera")
@@ -50,19 +51,44 @@ config.cameras.forEach((conf) => {
 
 
 // TMP grab 10 frames as performance test
-function doIt (i)
+// function doIt (i)
+// {
+//     if(i >= 10) return
+//     else {
+//         console.log((new Date) + "--------------------------------------------")
+//         Q.all(cameras.map(grabFrame))
+//         .then((frames) => {
+//             sharp(frames[0]).toFile("test-"+i+".jpg")
+//             .then(() => doIt(i+1))
+//         })
+//     }
+// }
+// doIt(0)
+
+// TMP
+function doIt(i)
 {
-    if(i >= 10) return
-    else {
-        console.log((new Date) + "--------------------------------------------")
-        generateComposite()
-        .then((composite) => {
-            composite.toFile("test-"+i+".jpg")
-        })
-        .then(() => doIt(i+1))
-    }
+    console.log(new Date + " START " + i + " --------------------") // TMP
+    return generateComposite()
+    .then((image) => {
+        console.log(new Date + " Writing to disk") // TMP
+        image.write("test-"+i+".jpg")
+        console.log(new Date + " DONE!") // TMP
+    })
+    .catch((err) => {
+        console.log("ERROR: "+err) // TMP
+    })
 }
 doIt(0)
+.then(() => doIt(1))
+.then(() => doIt(2))
+.then(() => doIt(3))
+.then(() => doIt(4))
+.then(() => doIt(5))
+.then(() => doIt(6))
+.then(() => doIt(7))
+.then(() => doIt(8))
+.then(() => doIt(9))
 
 // Helpers ---------------------------------------------------------------------
 
@@ -75,58 +101,58 @@ function cleanup ()
 }
 
 // Grab a frame from the given camera, with a Promise API.
-// :: (v4l2camera) -> Promise<Buffer(JPEG)>
+// :: (v4l2camera) -> Promise<Jimp, void>
 function grabFrame (cam)
 {
     return Q.Promise((resolve, reject) => {
         cam.capture((success) => {
-            if(success) resolve(new Buffer (cam.frameRaw()))
-            else reject()
+            if(!success) reject()
+            else {
+                // We need to use the Sharp library to parse the JPEG from
+                // v4l2camera, because:
+                // 1. It is an MJPEG frame, which is not entirely valid JPEG.
+                // 2. It seems to be slightly broken even if we patch it to JPEG.
+                sharp(new Buffer(cam.frameRaw()))
+                .jpeg().toBuffer()
+                .then((buffer) => {
+                    Jimp.read(buffer, (err, image) => {
+                        if(err) reject(err)
+                        else resolve(image)
+                    })
+                })
+            }
         })
     })
 }
 
 // Grabs a frame from all cameras, then composite them into one large image.
 // Returns a promise for the composite image.
-// :: (void) -> Promise<sharp>
+// :: (void) -> Promise<Jimp, anything>
 function generateComposite ()
 {
-    const outputWidth = config.outputWidth
-    const outputHeight = config.outputHeight
-
-    return Q.all(cameras.map(grabFrame))
-    .then((frames) => {
-        let canvas = createBlankImage(outputWidth, outputHeight)
-
-        let compositeBuffer = frames.reduce((base, overlay, i) => {
-            return base.then((buffer) => {
+    return createBlankImage(config.outputWidth, config.outputHeight)
+    .then((canvas) => {
+        return Q.all(cameras.map(grabFrame))
+        .then((frames) => {
+            frames.forEach((frame, i) => {
                 let cameraConfig = config.cameras[i]
-
-                return sharp(buffer)
-                .overlayWith(overlay, {
-                    top: cameraConfig.targetY,
-                    left: cameraConfig.targetX,
-                })
-                .jpeg().toBuffer()
+                canvas.blit(frame, cameraConfig.targetX, cameraConfig.targetY)
             })
-        }, canvas.jpeg().toBuffer())
 
-        return compositeBuffer.then((buffer) => sharp(buffer))
+            return canvas
+        })
     })
 }
 
-// Creates a blank canvas with a black background.
-// :: (int, int) -> sharp
-function createBlankImage (width, height)
+// Creates a blank canvas with optional background color (default=black).
+// :: (int, int) -> Promise<Jimp, error>
+// :: (int, int, int) -> Promise<Jimp, error>
+function createBlankImage (width, height, background=0x000000FF)
 {
-    const channels = 3
-    const buffer = Buffer.alloc(width * height * channels)
-
-    return sharp(buffer, {
-        raw: {
-            width: width,
-            height: height,
-            channels: channels,
-        }
+    return Q.Promise((resolve, reject) => {
+        new Jimp(width, height, background, (err, image) => {
+            if(err) reject(err)
+            else resolve(image)
+        })
     })
 }
