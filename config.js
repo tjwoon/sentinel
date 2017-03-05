e8ca9c7391be9e9d5ce2e387cac63db0c24645ca
TJ Woon <tj@cloudsky.org>
2017-03-05 12:06:19 +0800

    [Config] Capture at lower resolution for higher FPS

--- config.js
+++ config.js
@@ -7,11 +7,11 @@
 
 ***/
 
-const camHeight = 576
-const skip = 108
+const camHeight = 360
+const skip = 0
 
 module.exports = {
-    width: 1024, // All cameras must capture at this width!
+    width: 640, // All cameras must capture at this width!
     cameras: [
         {
             device: "/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.2:1.0-video-index0",
