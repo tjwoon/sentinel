eb74142ec508c5506cdb3aadec9c810f7db15436
TJ Woon <tj@cloudsky.org>
2017-04-24 12:55:24 +0800

    [Capture] [Update] Comments for config file

--- capture/config.js
+++ capture/config.js
@@ -1,5 +1,7 @@
 /***
 
+Note: This is not a JSON file because we want comments, variables, etc!
+
 - Nearest Ethernet, bottom:  platform-3f980000.usb-usb-0:1.3:1.0-video-index0
 - Nearest Ethernet, top:     platform-3f980000.usb-usb-0:1.2:1.0-video-index0
 - Furthest Ethernet, bottom: platform-3f980000.usb-usb-0:1.5:1.0-video-index0
