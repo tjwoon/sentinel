82fb28984bf304a8dd82e4cff978ee76848652d9
TJ Woon <tj@cloudsky.org>
2017-04-24 13:31:39 +0800

    [Capture] [Update] Added friendly constants to select USB port source

--- capture/config.js
+++ capture/config.js
@@ -2,14 +2,15 @@
 
 Note: This is not a JSON file because we want comments, variables, etc!
 
-- Nearest Ethernet, bottom:  platform-3f980000.usb-usb-0:1.3:1.0-video-index0
-- Nearest Ethernet, top:     platform-3f980000.usb-usb-0:1.2:1.0-video-index0
-- Furthest Ethernet, bottom: platform-3f980000.usb-usb-0:1.5:1.0-video-index0
-- Furthest Ethernet, top:    platform-3f980000.usb-usb-0:1.4:1.0-video-index0
 
 ***/
 
 const camHeight = 360
+const USB_NEAR_ETHERNET_BOTTOM = '/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.3:1.0-video-index0'
+const USB_NEAR_ETHERNET_TOP =    '/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.2:1.0-video-index0'
+const USB_FAR_ETHERNET_BOTTOM =  '/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.5:1.0-video-index0'
+const USB_FAR_ETHERNET_TOP =     '/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.4:1.0-video-index0'
+
 const skip = 0
 
 module.exports = {
