2dd3e8fd53a0b2ee97cbb62dba597ac10a969e2c
TJ Woon <tj@cloudsky.org>
2017-04-24 13:46:13 +0800

    [Capture] Updated config to have 2 cameras at slightly higher resolution
    With nothing else running on the Raspberry Pi 3, we can get 10-11 FPS at this resolution and capture framerate with no lag noticed in a short 10 second test

--- capture/config.js
+++ capture/config.js
@@ -12,33 +12,26 @@ $ v4l2-ctl -d /dev/v4l/by-path/platform-3f980000.usb-usb-0:1.3:1.0-video-index0 
 
 ***/
 
-const camHeight = 360
 const USB_NEAR_ETHERNET_BOTTOM = '/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.3:1.0-video-index0'
 const USB_NEAR_ETHERNET_TOP =    '/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.2:1.0-video-index0'
 const USB_FAR_ETHERNET_BOTTOM =  '/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.5:1.0-video-index0'
 const USB_FAR_ETHERNET_TOP =     '/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.4:1.0-video-index0'
 
+const camHeight = 448
 const skip = 0
 
 module.exports = {
-    width: 640, // All cameras must capture at this width!
+    width: 800, // All cameras must capture at this width!
     cameras: [
         {
-            device: "/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.2:1.0-video-index0",
+            device: USB_NEAR_ETHERNET_TOP,
             height: camHeight,
             skipTop: skip,
             skipBottom: skip,
             fps: 15,
         },
         {
-            device: "/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.3:1.0-video-index0",
-            height: camHeight,
-            skipTop: skip,
-            skipBottom: skip,
-            fps: 15,
-        },
-        {
-            device: "/dev/v4l/by-path/platform-3f980000.usb-usb-0:1.5:1.0-video-index0",
+            device: USB_NEAR_ETHERNET_BOTTOM,
             height: camHeight,
             skipTop: skip,
             skipBottom: skip,
